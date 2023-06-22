5a6ff25c394841f55d9608cf351b2e79c0e6d13e
ALESSANDRO FERNANDES ROCHA JUNIOR <alessandro.6957@aluno.pr.senac.br>
2023-06-22 11:47:07 -0300

    salvar

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
           </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              CADASTRAR
+              ENTRAR/CADASTRAR
             </a>
           </li>
 
@@ -101,6 +101,13 @@
 
     </div>
   </div>
+  <br>
+  <br>
+  <br>
+  <h1>Lançamentos</h1>
+  <div id="lançamento">
+    <img src="img-projeto/tout-gamma-force-05.06-desk-v2.avif" alt="...">
+</div>
  
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>

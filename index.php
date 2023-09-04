eb09409ee6f12fd510f974e44b54320641677172
Dieimes Nunes de Souza <dieimes.souza@docente.pr.senac.br>
2023-09-04 11:26:00 -0300

    Atualização

--- index.php
+++ index.php
@@ -1,80 +1,16 @@
 <!doctype html>
 <html lang="pt-br">
-
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>𝕭𝖑𝖆𝖈𝖐 𝕯𝖔𝖌 𝕴𝖓𝖎𝖈𝖎𝖆𝖑</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
-  <link rel="stylesheet" href="CSS/camisa.css">
- 
+  <link rel="stylesheet" href="CSS/dieimes.css">
 </head>
 
 <body>
-
-<nav class="navbar bg-body-tertiary fixed-top">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="masculino.php"><img id="bicho" src="img talarico/BackgroundEraser_20230811_081558768.png" width="65" height="59"><h1>𝕭𝖑𝖆𝖈𝖐 𝕯𝖔𝖌</h1></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-      <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">𝕭𝖑𝖆𝖈𝖐 𝕯𝖔𝖌</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div class="offcanvas-body">
-        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">𝕹𝖔𝖛𝖎𝖉𝖆𝖉𝖊𝖘</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">𝕾𝖔𝖇𝖗𝖊 𝕹𝖔́𝖘</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
-</svg> Minha Conta
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="cadastro.html">Entrar</a></li>
-              <li><a class="dropdown-item" href="#">Cadastrar</a></li>
-              <li>
-                <hr class="dropdown-divider">
-              </li>
-              <li><a class="dropdown-item" href="masculino.php">Roupas</a></li>
-            </ul>
-          </li>
-        </ul>
-        <form class="d-flex mt-3" role="search">
-          <input class="form-control me-2" type="Procurar..." placeholder="𝕻𝖊𝖘𝖖𝖚𝖎𝖘𝖆𝖗..." aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">𝕻𝖗𝖔𝖈𝖚𝖗𝖆𝖗</button>
-        </form>
-      </div>
-    </div>
-  </div>
-</nav>
-<br>
-<br>
-<br>
-<br>
+  <?php include("menu.php") ?>
   <div class="container">
-  <br>
-
-  <div id="camiseta">
-  <div id="carouselExampleCaptions" class="carousel slide">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  
-  <br>
-
-  <div class="container">
-
     <div class="row">
       <div class="col">
         <div class="card" style="width: 18rem;">
@@ -90,7 +26,6 @@
           </div>
         </div>
       </div>
-
       <div class="col">
         <div class="card" style="width: 18rem;">
           <img src="img-projeto2/pequeno1.png" class="img-fluid" alt="...">
@@ -104,7 +39,6 @@
           </div>
         </div>
       </div>
-
       <div class="col">
         <div class="card" style="width: 18rem;">
           <img src="img-projeto2/barca1.png" class="img-fluid" alt="...">
@@ -114,31 +48,14 @@
             <p class="card-text"> Masculina</p>
             <p>Futebol</p>
             <p>R$ 139,49</p>
-            
+
             <a href="barcelona.php" class="btn btn-dark">Comprar Agora</a>
 
           </div>
         </div>
       </div>
-
-    </div>
+    </div>    
+      <img src="img talarico/img5.png" width="1100" height="760">   
   </div>
-  <br>
-  <br>
-  <br>
-  <div id="lançamento">
-    <center><img src="img talarico/img5.png" width="1100" height="760"></center>
-  </div>
-  </div>
-            </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </footer>
-
- 
-</body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </html>
---
+++ menu.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+    <div class="container-fluid">
+        <a class="navbar-brand d-flex align-items-center" href="masculino.php">
+            <img id="bicho" src="img talarico/BackgroundEraser_20230811_081558768.png" width="65" height="59" class="me-2">
+            <h1>Black Dog</h1>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Novidades</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Sobre Nós</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Minha Conta
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end">
+                        <li><a class="dropdown-item" href="cadastro.html">Entrar</a></li>
+                        <li><a class="dropdown-item" href="#">Cadastrar</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="masculino.php">Roupas</a></li>
+                    </ul>
+                </li>
+            </ul>            
+        </div>
+    </div>
+</nav>

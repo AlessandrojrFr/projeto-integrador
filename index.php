729d3ec1eaf33ab75f764f203d68857bcb7de6f8
JOAO PEDRO TALARICO ANDRE <andre.5970@aluno.pr.senac.br>
2023-09-11 08:29:17 -0300

    salvar e logo cccccc

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minha Loja</title>
+    <link rel="stylesheet" href="CSS/styles.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Início</a></li>
+                <li><a href="shop.php">Compras</a></li>
+                <li><a href="checkout.php">Pagamento</a></li>
+                <li><a href="roupas.html">Roupas</a></li>
+                <li><a href="minhaconta.php">Minha Conta</a></li>
+                <li><a href="sobrenos.php">Sobre Nós</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+    <div class="content">
+        <h1>Bem-vindo à Minha Loja</h1>
+        <p>Oferecemos uma variedade de produtos incríveis para você.</p>
+    </div>
+    
+    
+    <div class="content">
+        <h1>Bem-vindo ao Mundo dos Esportes</h1>
+        <p>Encontre as melhores roupas e sapatos esportivos aqui!</p>
+    </div>
+
+    <!-- Seção de destaque de produtos -->
+    <div class="featured-products">
+        <div class="product">
+            <img src="imagens_compras/02614115.avif" alt="Camisa de Futebol">
+            <h3>Camisa de Futebol</h3>
+            <p>Camisa oficial do time Barcelona</p>
+            <p>Preço: R$99.00</p>
+        </div>
+        <div class="product">
+            <img src="imagens_compras/025231ID.avif" alt="Tênis de Corrida">
+            <h3>Tênis de Corrida</h3>
+            <p>Ideal para treinamentos de corrida</p>
+            <p>Preço: R$129.00</p>
+        </div>
+        <!-- Adicione mais produtos em destaque aqui -->
+    </div>
+    
+
+    
+<footer>
+    <div class="footer-content">
+        <div class="footer-section about">
+            <h2>Sobre Nós</h2>
+            <p>Somos uma loja dedicada a fornecer os melhores produtos esportivos para atletas e entusiastas do esporte. Nossos produtos são selecionados cuidadosamente para garantir qualidade e desempenho.</p>
+        </div>
+        <div class="footer-section social">
+            <h2>Redes Sociais</h2>
+            <a href="#"><i class="fab fa-facebook"></i> Facebook</a>
+            <a href="#"><i class="fab fa-twitter"></i> Twitter</a>
+            <a href="#"><i class="fab fa-instagram"></i> Instagram</a>
+        </div>
+        <div class="footer-section contact">
+            <h2>Contato</h2>
+            <p>Email: contato@lojaesportiva.com</p>
+            <p>Telefone: (123) 456-7890</p>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        &copy; 2023 Loja Esportiva | Todos os direitos reservados
+    </div>
+</footer>
+
+</body>
+</html>

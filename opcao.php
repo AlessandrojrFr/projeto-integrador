5dc0bb938f86cd83cc2426ce19c2b89f15f0c30b
MATHEUS ZANONI BITTENCOURT DE OLIVEIRA <oliveira.4942@aluno.pr.senac.br>
2023-09-22 08:44:30 -0300

    att

---
+++ opcao.php
@@ -0,0 +1,23 @@
+<?php
+  if (empty($opcaoSelecionada)) 
+    echo "Nenhuma opção selecionada.";
+if(isset($_POST['metodo'])){
+    $metodo = $_POST['metodo'];
+    
+    switch($metodo){
+        case 'cartao':
+            header("Location: cartao.php");
+            break;
+        case 'pix':
+            header("Location: pix.php");
+            break;
+        case 'boleto':
+            header("Location: boleto.php");
+            break;
+        default:
+            header("Location: metodo_pag.php");
+            break;
+    }
+    exit();
+}
+?>

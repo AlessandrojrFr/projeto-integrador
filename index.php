d3cf25a98d49cb4a8c44233f74467a04b060af7e
ALESSANDRO FERNANDES ROCHA JUNIOR <alessandro.6957@aluno.pr.senac.br>
2023-09-25 09:02:58 -0300

    queo sei la branch

--- index.php
+++ index.php
@@ -3,82 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Minha Loja</title>
-    <link rel="stylesheet" href="CSS/styles.css">
+    <title>Login de Usuário</title>
+    <link rel="stylesheet" href="CSS/login2.css">
 </head>
 <body>
-    <header>
-    <nav class="navbar">
-    <div class="navbar-left">
-        <!-- Seu logo aqui -->
-        <img src="chat/logo (1).png" alt="Seu Logo">
-    </div>
 
-</nav>
+    <div class="login-container">
+        <h2>Login de Usuário</h2>
+        
+        <!-- Espaço para foto do perfil -->
+        <div class="profile-picture">
+            <img src="chat/simpsons3.png" alt="Foto de perfil">
+        </div>
 
-        <nav>
-            <ul>
-                <li><a href="contato.php">Contato</a></li>
-                <li><a href="shop.php">Compras</a></li>
-                <li><a href="sobrenos.php">Sobre Nós</a></li>
-                <li><a href="minhaconta.php">Minha Conta</a></li>
-                <li><a href="entrar_cadastrar.php">Entrar / Cadastrar</a></li>
-            </ul>
-        </nav>
-    </header>
-    
-    <div class="content">
-        <h1>Bem-vindo a Minha Loja</h1>
-        <p>Oferecemos uma variedade de produtos incríveis para você.</p>
-    </div>
-    
-    
-    <div class="content">
-        <h1>Bem-vindo ao Mundo dos Esportes</h1>
-        <p>Encontre as melhores roupas e sapatos esportivos aqui!</p>
-    </div>
+        <!-- Formulário de login -->
+        <form action="#" method="post">
+            <label for="email">E-mail:</label>
+            <input class="inputa" type="email" id="email" name="email" required>
 
-    <!-- Seção de destaque de produtos -->
-    <div class="featured-products">
-        <div class="product">
-            <img src="chat/roupa2.png" alt="Camisa de Futebol">
-            <h3>Camisa de Futebol</h3>
-            <p>Camisa oficial do time Barcelona</p>
-            <p>Preço: R$99.00</p>
-        </div>
-        <div class="product">
-            <img src="chat/roupa1.png" alt="Tênis de Corrida">
-            <h3>Tênis de Corrida</h3>
-            <p>Ideal para treinamentos de corrida</p>
-            <p>Preço: R$129.00</p>
-        </div>
-        <!-- Adicione mais produtos em destaque aqui -->
+            <label for="password">Senha:</label>
+            <input class="inputa" type="password" id="password" name="password" required>
+            <button class="btn btn-primary"><a id="link-button" href="restrita.php">Entrar</a></button>
+        </form>
+        
     </div>
-    
-
-    
-<footer>
-    <div class="footer-content">
-        <div class="footer-section about">
-            <h2>Sobre Nós</h2>
-            <p>Somos uma loja dedicada a fornecer os melhores produtos esportivos para atletas e entusiastas do esporte. Nossos produtos são selecionados cuidadosamente para garantir qualidade e desempenho.</p>
-        </div>
-        <div class="footer-section social">
-            <h2>Redes Sociais</h2>
-            <a href="#"><i class="fab fa-facebook"></i> Facebook</a>
-            <a href="#"><i class="fab fa-twitter"></i> Twitter</a>
-            <a href="#"><i class="fab fa-instagram"></i> Instagram</a>
-        </div>
-        <div class="footer-section contact">
-            <h2>Contato</h2>
-            <p>Email: contato@lojaesportiva.com</p>
-            <p>Telefone: (123) 456-7890</p>
-        </div>
-    </div>
-    <div class="footer-bottom">
-        &copy; 2023 Loja Esportiva | Todos os direitos reservados
-    </div>
-</footer>
 
 </body>
 </html>
--- restrita.php
+++ restrita.php
@@ -6,18 +6,60 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Loja de Roupas Esportivas</title>
     <link rel="stylesheet" href="css/novidade.css">
+    <link rel="stylesheet" href="CSS/styles.css">
 </head>
 <body>
+
+
 <header>
+    <nav class="navbar">
+    <div class="navbar-left">
+        <!-- Seu logo aqui -->
+        <img src="chat/logo (1).png" alt="Seu Logo">
+    </div>
+
+</nav>
+
         <nav>
-            <a href="index.php">Início</a>
-            <a href="shop.php">Compras</a>
-            <a href="contato.php">Contato</a>
-            <a href="minhaconta.php">Minha Conta</a>
-            <a href="novidades.php">Novidades</a>
-            <a href="consulta.php">Horário de Consulta</a>
+            <ul>
+                <li><a href="contato.php">Contato</a></li>
+                <li><a href="shop.php">Compras</a></li>
+                <li><a href="sobrenos.php">Sobre Nós</a></li>
+                <li><a href="minhaconta.php">Minha Conta</a></li>
+                <li><a href="entrar_cadastrar.php">Entrar / Cadastrar</a></li>
+            </ul>
         </nav>
     </header>
+    
+    <div class="content">
+        <h1>Bem-vindo a Minha Loja</h1>
+        <p>Oferecemos uma variedade de produtos incríveis para você.</p>
+    </div>
+    
+    
+    <div class="content">
+        <h1>Bem-vindo ao Mundo dos Esportes</h1>
+        <p>Encontre as melhores roupas e sapatos esportivos aqui!</p>
+    </div>
+
+    <!-- Seção de destaque de produtos -->
+    <div class="featured-products">
+        <div class="product">
+            <img src="chat/roupa2.png" alt="Camisa de Futebol">
+            <h3>Camisa de Futebol</h3>
+            <p>Camisa oficial do time Barcelona</p>
+            <p>Preço: R$99.00</p>
+            <button>Comprar Agora</button>
+        </div>
+        <div class="product">
+            <img src="chat/roupa1.png" alt="Tênis de Corrida">
+            <h3>Tênis de Corrida</h3>
+            <p>Ideal para treinamentos de corrida</p>
+            <p>Preço: R$129.00</p>
+            <button>Comprar Agora</button>
+        </div>
+        <!-- Adicione mais produtos em destaque aqui -->
+    </div>
 
 
     
@@ -30,12 +72,14 @@
                 <img src="chat/roupa1.png" alt="Novidade 2">
                 <h3>Lançamento de Calçados Esportivos</h3>
                 <p>Explore nossa linha de calçados esportivos de alta performance, projetados para atletas exigentes.</p>
+                <button>Comprar Agora</button>
             </div>
 
             <div class="promo-item">
                 <img src="chat/roupa3.png" alt="Promoção 2">
                 <h3>Camisetas Polo por R$ 39,90</h3>
                 <p>Camisetas polo elegantes por apenas R$ 39,90 cada. Não perca essa oportunidade!</p>
+                <button>Comprar Agora</button>
             </div>
             
             <!-- Adicione mais promoções conforme necessário -->
@@ -47,12 +91,14 @@
                 <img src="chat/femininobrasil4.png" alt="Novidade 1">
                 <h3>Nova Coleção de Roupas Femininas</h3>
                 <p>Confira nossa mais recente coleção de roupas esportivas femininas, perfeitas para seu estilo ativo.</p>
+                <button>Comprar Agora</button>
             </div>
 
             <div class="promo-item">
                 <img src="chat/chuteira.png" alt="Promoção 1">                                                                                                                                              
                 <h3>Desconto de 30% em Chuteiras</h3>
                 <p>Aproveite agora para comprar sua chuteira favorita com um desconto especial de 30%!</p>
+                <button>Comprar Agora</button>
             </div>
             
             <!-- Adicione mais novidades conforme necessário -->
@@ -67,12 +113,26 @@
             <button onclick="finalizarCompra()">Finalizar Compra</button>
         </section>
     </main>
-
+    
 
     <footer>
-        <p>&copy; 2023 - Loja de Roupas Esportivas</p>
-    </footer>
-
+    <div class="footer-content">
+        <div class="footer-section about">
+            <h2>Sobre Nós</h2>
+            <p>Somos uma loja dedicada a fornecer os melhores produtos esportivos para atletas e entusiastas do esporte. Nossos produtos são selecionados cuidadosamente para garantir qualidade e desempenho.</p>
+        </div>
+        <div class="footer-section social">
+            <h2>Redes Sociais</h2>
+            <a href="#"><i class="fab fa-facebook"></i> Facebook</a>
+            <a href="#"><i class="fab fa-twitter"></i> Twitter</a>
+            <a href="#"><i class="fab fa-instagram"></i> Instagram</a>
+        </div>
+        <div class="footer-section contact">
+            <h2>Contato</h2>
+            <p>Email: contato@lojaesportiva.com</p>
+            <p>Telefone: (123) 456-7890</p>
+        </div>
+    </div>
     <script src="script.js"></script>
 </body>
 </html>

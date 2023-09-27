619fd5fa5a0c2d4af3549e9f8076e7d128fca43f
ALESSANDRO FERNANDES ROCHA JUNIOR <alessandro.6957@aluno.pr.senac.br>
2023-09-27 08:43:09 -0300

    pãpo

--- restrita.php
+++ restrita.php
@@ -1,35 +1,34 @@
 
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Loja de Roupas Esportivas</title>
-    <link rel="stylesheet" href="css/novidade.css">
-    <link rel="stylesheet" href="CSS/styles.css">
+    <title>Navbar com Dropdown</title>
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-
-
-<header>
     <nav class="navbar">
-    <div class="navbar-left">
-        <!-- Seu logo aqui -->
-        <img src="chat/logo (1).png" alt="Seu Logo">
-    </div>
+        <ul>
+            <li><a href="#">Contato</a></li>
+            <li><a href="#">Compras</a></li>
+            <li><a href="#">Sobre Nós</a></li>
+            <li><a href="#">Consulta</a></li>
+            <li><a href="#">Novidades</a></li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle">Minha Conta &#9662;</a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Configurações</a></li>
+                    <li><a href="#">Meus Pedidos</a></li>
+                    <li><a href="#">Sair</a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+    <script src="script2.js"></script>
 
-</nav>
 
-        <nav>
-            <ul>
-                <li><a href="contato.php">Contato</a></li>
-                <li><a href="shop.php">Compras</a></li>
-                <li><a href="sobrenos.php">Sobre Nós</a></li>
-                <li><a href="minhaconta.php">Minha Conta</a></li>
-                <li><a href="entrar_cadastrar.php">Entrar / Cadastrar</a></li>
-            </ul>
-        </nav>
-    </header>
+
     
     <div class="content">
         <h1>Bem-vindo a Minha Loja</h1>
@@ -49,19 +48,74 @@
             <h3>Camisa de Futebol</h3>
             <p>Camisa oficial do time Barcelona</p>
             <p>Preço: R$99.00</p>
-            <button>Comprar Agora</button>
         </div>
         <div class="product">
             <img src="chat/roupa1.png" alt="Tênis de Corrida">
             <h3>Tênis de Corrida</h3>
             <p>Ideal para treinamentos de corrida</p>
             <p>Preço: R$129.00</p>
-            <button>Comprar Agora</button>
         </div>
         <!-- Adicione mais produtos em destaque aqui -->
     </div>
+    
+
+    
+<footer>
+    <div class="footer-content">
+        <div class="footer-section about">
+            <h2>Sobre Nós</h2>
+            <p>Somos uma loja dedicada a fornecer os melhores produtos esportivos para atletas e entusiastas do esporte. Nossos produtos são selecionados cuidadosamente para garantir qualidade e desempenho.</p>
+        </div>
+        <div class="footer-section social">
+            <h2>Redes Sociais</h2>
+            <a href="#"><i class="fab fa-facebook"></i> Facebook</a>
+            <a href="#"><i class="fab fa-twitter"></i> Twitter</a>
+            <a href="#"><i class="fab fa-instagram"></i> Instagram</a>
+        </div>
+        <div class="footer-section contact">
+            <h2>Contato</h2>
+            <p>Email: contato@lojaesportiva.com</p>
+            <p>Telefone: (123) 456-7890</p>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        &copy; 2023 Loja Esportiva | Todos os direitos reservados
+    </div>
+</footer>
 
 
+
+
+
+    
+    <div class="content">
+        <h1>Bem-vindo a Minha Loja</h1>
+        <p>Oferecemos uma variedade de produtos incríveis para você.</p>
+    </div>
+    
+    
+    <div class="content">
+        <h1>Bem-vindo ao Mundo dos Esportes</h1>
+        <p>Encontre as melhores roupas e sapatos esportivos aqui!</p>
+    </div>
+
+    <!-- Seção de destaque de produtos -->
+    <div class="featured-products">
+        <div class="product">
+            <img src="chat/roupa2.png" alt="Camisa de Futebol">
+            <h3>Camisa de Futebol</h3>
+            <p>Camisa oficial do time Barcelona</p>
+            <p>Preço: R$99.00</p>
+        </div>
+        <div class="product">
+            <img src="chat/roupa1.png" alt="Tênis de Corrida">
+            <h3>Tênis de Corrida</h3>
+            <p>Ideal para treinamentos de corrida</p>
+            <p>Preço: R$129.00</p>
+        </div>
+        <!-- Adicione mais produtos em destaque aqui -->
+    </div>
+    
     
     <main class="news">
         <h1>Novidades e Promoções</h1>
@@ -133,6 +187,10 @@
             <p>Telefone: (123) 456-7890</p>
         </div>
     </div>
-    <script src="script.js"></script>
+    <div class="footer-bottom">
+        &copy; 2023 Loja Esportiva | Todos os direitos reservados
+    </div>
+</footer>
+    <script src="css/script.js"></script>
 </body>
 </html>

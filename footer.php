69b3739be0c3efd90ce52e684d15c09a7c9be484
Dieimes Nunes de Souza <dieimes.souza@docente.pr.senac.br>
2023-10-04 11:50:06 -0300

    Atualização

--- footer.php
+++ footer.php
@@ -1,81 +1,55 @@
-<!DOCTYPE html>
-<html lang="pt-br">
+ <footer class="main_footer container">
 
- <head>
-	
-	<meta charset="utf-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <meta name="Loop Nerd" content="Códigos Pronto" />
-    <meta name="description" content="Loop Nerd Códigos e sites pronto para você utilizar no seu negócio."/>
-    <meta name="keywords" content="Footer Responsivo, Footer, Rodape Responsivo, Códigos Html Prontos, Códigos Css Prontos."/>
-    <meta name="author" content="Loop Nerd" />
-    <meta name="URL" content="https://www.www.loopnerd.com"/>
-    <meta http-equiv="content-language" content="pt-br" />
-    <meta name="robots" content="index, follow"/>
-    
-    <!--[if lt IE 9]>
-        <script src="js/html5shiv.js"></script>
-    <![endif]-->       
+     <div class="content">
 
-	<link rel="stylesheet" type="text/css" href="css2/reset.css">
-	<link rel="stylesheet" type="text/css" href="css2/footer.css">
-    <link rel="stylesheet" type="text/css" href="css2/fonts-icones.css">
-    <link rel="shortcut icon" href="https://www.loopnerd.com.br/img/favicon.png" type="image/ico" />
+         <div class="colfooter">
 
-<footer class="main_footer container">
-    
-    <div class="content">
+             <h3 class="titleFooter"> Menu</h3>
 
-        <div class="colfooter">
-            
-            <h3 class="titleFooter"> Menu</h3>
-            
-            <ul>
-            
-              <li><a href="#" title="Página Inícial">Página Inícial</a></li>
-              <li><a href="#" title="Sobre a Empresa">Sobre a Empresa</a></li>
-              <li><a href="#" title="Galeria de Fotos">Galeria de Fotos</a></li>
-              <li><a href="#" title="Fale Conosco">Fale Conosco</a></li>
-            
-            </ul>
+             <ul>
 
-        </div><!--Col Footer 1-->        
+                 <li><a href="#" title="Página Inícial">Página Inícial</a></li>
+                 <li><a href="#" title="Sobre a Empresa">Sobre a Empresa</a></li>
+                 <li><a href="#" title="Galeria de Fotos">Galeria de Fotos</a></li>
+                 <li><a href="#" title="Fale Conosco">Fale Conosco</a></li>
 
-        <div class="colfooter">
-           
-           <h3 class="titleFooter"> Contato</h3>
-           <p><i class="icon icon-mail"></i> Simpsans@gmail.com</p>
-           <p><i class="icon icon-phone"></i> (43) 3472-0001</p>
-           <p><i class="icon icon-whatsapp"></i> (43) 99840-4433</p>
+             </ul>
 
-        </div><!--Col Footer 2-->
+         </div><!--Col Footer 1-->
 
-        <div class="colfooter">
-           
-           <h3 class="titleFooter"> Redes Sociais</h3>
-            
-           <a href="#" class="botao"><span> <i class="icon icon-facebook"></i> </span></a>            
-           <a href="#" class="botao"><span> <i class="icon icon-instagram"></i> </span></a>            
-           <a href="#" class="botao"><span> <i class="icon icon-twitter"></i> </span></a>            
-           <a href="#" class="botao"><span> <i class="icon icon-pinterest"></i> </span></a>            
+         <div class="colfooter">
 
-        </div><!--Col Footer 3-->
+             <h3 class="titleFooter"> Contato</h3>
+             <p><i class="icon icon-mail"></i> Simpsans@gmail.com</p>
+             <p><i class="icon icon-phone"></i> (43) 3472-0001</p>
+             <p><i class="icon icon-whatsapp"></i> (43) 99840-4433</p>
+
+         </div><!--Col Footer 2-->
+
+         <div class="colfooter">
+
+             <h3 class="titleFooter"> Redes Sociais</h3>
+
+             <a href="#" class="botao"><span> <i class="icon icon-facebook"></i> </span></a>
+             <a href="#" class="botao"><span> <i class="icon icon-instagram"></i> </span></a>
+             <a href="#" class="botao"><span> <i class="icon icon-twitter"></i> </span></a>
+             <a href="#" class="botao"><span> <i class="icon icon-pinterest"></i> </span></a>
+
+         </div><!--Col Footer 3-->
 
 
-        <div class="clear"></div>
-    
-    </div><!--Contant-->
+         <div class="clear"></div>
 
-    <div class="main_footer_copy">
-        
-        <p class="m-b-footer"> Seusite - 2021, todos os direitos reservados.</p> 
-        <p class="by"><i class="icon icon-heart-3"></i> Desenvolvido por: <a href="#" title="Seu nome">PROJETO HOMER</a></p>
-    
-    </div>
+     </div><!--Contant-->
 
-</footer>
+     <div class="main_footer_copy">
 
-<script src="js/jquery.js"></script>
-<script src="js/script.js"></script>
-</body>
-</html>
+         <p class="m-b-footer"> Seusite - 2021, todos os direitos reservados.</p>
+         <p class="by"><i class="icon icon-heart-3"></i> Desenvolvido por: <a href="#" title="Seu nome">PROJETO HOMER</a></p>
+
+     </div>
+
+ </footer>
+
+ <script src="js/jquery.js"></script>
+ <script src="js/script.js"></script>
--- index.php
+++ index.php
@@ -5,17 +5,23 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title></title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" type="text/css" href="css2/footer.css">
+  <link rel="stylesheet" type="text/css" href="css2/fonts-icones.css">
+  <link rel="shortcut icon" href="https://www.loopnerd.com.br/img/favicon.png" type="image/ico" />
 </head>
 <body>
   <?php
-  include("navbar.php")
+  include("navbar.php");
   ?>
 
   <br></br><br></br><br></br><br></br><br></br><br></br>
   
-      <?php
-            include ("footer.php")
-      ?>
+  <?php
+    include ("footer.php");
+  ?>
 
 </body>
 
